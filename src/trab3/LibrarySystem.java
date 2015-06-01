439c45c1f3ee78b01f0fbed46359d5121e051e32
Victor Luiz <Victor Luiz@172.26.224.148>
2015-06-01 11:07:02 -0300

    Adicionado atualização de data e modificado menu

--- src/trab3/LibrarySystem.java
+++ src/trab3/LibrarySystem.java
@@ -3,102 +3,72 @@ package trab3;
 import java.util.Calendar;
 import java.util.Scanner;
 
+public class LibrarySystem {
 
-public class LibrarySystem{
-	static Biblioteca library;
+    static Biblioteca library;
+
     public static void main(String args[]) {
 
- 
 	library = new Biblioteca();
+
 	String bookFile = "src/books.csv";
 	String userFile = "src/users.csv";
 	String rentFile = "src/rents.csv";
 
 	library.updateLib(bookFile, userFile, rentFile);
-	
+
 	int resp = 0;
-	while(resp != 4){
-		resp = Menu();
-		if (resp == 1){
-			resp = MenuCadastrar();
-			if ( resp == 1){
-				resp = MenuCadastrarUsuario();
-				if ( resp == 1){
-					MenuCadastrarComunidade();
-				}
-				else if ( resp == 2){
-					resp = MenuCadastrarAluno();
-				}
-				else if ( resp == 3){
-					resp = MenuCadastrarProfessor();
-				}
+	while (resp != 4) {
+	    resp = Menu();
+	    if (resp == 1) {
+		resp = 0;
+		while (resp != 3) {
+		    resp = MenuCadastrar();
+		    if (resp == 1) {
+			resp = 0;
+			while (resp != 4) {
+			    resp = MenuCadastrarUsuario();
 			}
-			else if ( resp == 2){
-				resp = MenuCadastrarLivro();
+		    } else if (resp == 2) {
+			resp = 0;
+			while (resp != 3) {
+			    resp = MenuCadastrarLivro();
 			}
+		    }
 		}
-		else if ( resp == 2){
+	    } else if (resp == 2) {
+		resp = 0;
+		while (resp != 1) {
+
+		}
+	    } else if (resp == 3) {
+		resp = 0;
+		while (resp != 3) {
+		    resp = MenuData();
+		    if (resp == 1) {
+			Calendar c = Calendar.getInstance();
+			System.out.println("Data e Hora atual: " + c.getTime());
+		    } else if (resp == 2) {
+			Calendar c = Calendar.getInstance();
 			
+			System.out.println("Entre com o dia (dd):");
+			Scanner in = new Scanner(System.in);
+			c.set(Calendar.DAY_OF_MONTH, in.nextInt());
+
+			System.out.println("Entre com o mês (mm):");
+			in = new Scanner(System.in);
+			c.set(Calendar.MONTH, (in.nextInt()-1));
+
+			System.out.println("Entre com o ano (yyyy):");
+			in = new Scanner(System.in);
+			c.set(Calendar.YEAR, in.nextInt());
+
+			System.out.println("Data e Hora atual: " + c.getTime());
+			in.close();
+		    }
 		}
-		else if ( resp == 3){
-			
-		}
+	    }
 	}
-
-
-	
-	Calendar day = Calendar.getInstance();
-	//day.set(Integer.parseInt(dates[0]), Integer.parseInt(dates[1]), Integer.parseInt(dates[2]));
-	
-    }
-    
-    static int Menu(){
-    	System.out.println("Bem vindo ao sistema de biblioteca");
-    	System.out.println("\n[1] Cadastrar");
-    	System.out.println("[2] Listar registros");
-    	System.out.println("[3] Simular outra data");
-    	System.out.println("[4] Sair");
-    	Scanner in = new Scanner(System.in);
-    	int resp = in.nextInt();
-    	in.close();
-    	return resp;    	
-    }
-    
-    static int MenuCadastrar(){
-    	System.out.println("O que deseja cadastrar :");
-    	System.out.println("[1] Usuário");
-    	System.out.println("[2] Livro");
-    	System.out.println("[3] Voltar");
-
-    	Scanner in = new Scanner(System.in);
-    	int resp = in.nextInt();
-    	in.close();
-    	return resp;    	
-    }
-    
-    static int MenuCadastrarUsuario(){
-    	System.out.println("Qual o tipo de usuário ?");
-    	System.out.println("[1] Comunidade");
-    	System.out.println("[2] Aluno");
-    	System.out.println("[3] Professor");
-    	System.out.println("[4] Sair");
-
-    	Scanner in = new Scanner(System.in);
-    	int resp = in.nextInt();
-    	in.close();
-    	return resp; 
-    }
-    
-    static int MenuCadastrarLivro(){
-    	System.out.println("Qual o tipo de livro ?");
-    	System.out.println("[1] Texto");
-    	System.out.println("[2] Livro padrão");
-    	System.out.println("[3] Sair");
-
-    	Scanner in = new Scanner(System.in);
-    	int resp = in.nextInt();
-    	in.close();
-    	return resp; 
     }
     
     static void MenuCadastrarComunidade(){
@@ -159,4 +129,71 @@ public class LibrarySystem{
     	
     }
 
+    static int Menu() {
+	System.out.println("\nBem vindo ao sistema de biblioteca");
+	System.out.println("\nMenu:");
+	System.out.println("[1] Cadastrar");
+	System.out.println("[2] Listar registros");
+	System.out.println("[3] Configurar data");
+	System.out.println("[4] Sair");
+	int resp;
+	try (Scanner in = new Scanner(System.in)) {
+	    resp = in.nextInt();
+	}
+	return resp;
+    }
+
+    static int MenuCadastrar() {
+	System.out.println("O que deseja cadastrar :");
+	System.out.println("[1] Usuário");
+	System.out.println("[2] Livro");
+	System.out.println("[3] Sair");
+
+	int resp;
+	try (Scanner in = new Scanner(System.in)) {
+	    resp = in.nextInt();
+	}
+	return resp;
+    }
+
+    static int MenuCadastrarUsuario() {
+	System.out.println("Qual o tipo de usuário ?");
+	System.out.println("[1] Comunidade");
+	System.out.println("[2] Aluno");
+	System.out.println("[3] Professor");
+	System.out.println("[4] Sair");
+
+	int resp;
+	try (Scanner in = new Scanner(System.in)) {
+	    resp = in.nextInt();
+	}
+	return resp;
+    }
+
+    static int MenuCadastrarLivro() {
+	System.out.println("Qual o tipo de livro ?");
+	System.out.println("[1] Texto");
+	System.out.println("[2] Livro padrão");
+	System.out.println("[3] Sair");
+
+	int resp;
+	try (Scanner in = new Scanner(System.in)) {
+	    resp = in.nextInt();
+	}
+	return resp;
+    }
+
+    static int MenuData() {
+	System.out.println("Configuração de data:");
+	System.out.println("[1] Deixar data atual");
+	System.out.println("[2] Modificar data");
+	System.out.println("[3] Sair");
+
+	int resp;
+	try (Scanner in = new Scanner(System.in)) {
+	    resp = in.nextInt();
+
+	}
+	return resp;
+    }
 }
